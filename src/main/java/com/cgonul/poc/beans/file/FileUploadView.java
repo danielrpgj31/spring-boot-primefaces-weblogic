180f15eadddd7a4305b4e9d37f13859536f8b3d7
Caglar Gonul <cgonul@netas.com.tr>
2016-12-08 16:55:05 +0300

    master File upload component has been implemented.

---
+++ src/main/java/com/cgonul/poc/beans/file/FileUploadView.java
@@ -0,0 +1,32 @@
+package com.cgonul.poc.beans.file;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+
+import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.UploadedFile;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component("fileUploadView")
+@Scope("view")
+public class FileUploadView {
+
+    private UploadedFile file;
+
+    public UploadedFile getFile() {
+        return file;
+    }
+
+    public void setFile(UploadedFile file) {
+        this.file = file;
+    }
+
+    public void upload() {
+        if(file != null) {
+            FacesMessage message = new FacesMessage("Succesful", file.getFileName() + " is uploaded.");
+            FacesContext.getCurrentInstance().addMessage(null, message);
+        }
+    }
+}
