6e1a38feaef9a0cd395518b93fbcaba8505f88e7
Caglar Gonul <cgonul@netas.com.tr>
2016-12-08 15:01:52 +0300

    master Lazy data table has been implemented.

---
+++ src/main/java/com/cgonul/poc/beans/data/datatable/LazyView.java
@@ -0,0 +1,44 @@
+package com.cgonul.poc.beans.data.datatable;
+
+import org.primefaces.event.SelectEvent;
+import org.primefaces.model.LazyDataModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+@Component("dtLazyView")
+@Scope("view")
+public class LazyView {
+    private LazyDataModel<Car> lazyModel;
+
+    private Car selectedCar;
+
+    @Autowired
+    private CarService service;
+
+    @PostConstruct
+    public void init() {
+        lazyModel = new LazyCarDataModel(service.createCars(200));
+    }
+
+    public LazyDataModel<Car> getLazyModel() {
+        return lazyModel;
+    }
+
+    public Car getSelectedCar() {
+        return selectedCar;
+    }
+
+    public void setSelectedCar(Car selectedCar) {
+        this.selectedCar = selectedCar;
+    }
+
+    public void onRowSelect(SelectEvent event) {
+        FacesMessage msg = new FacesMessage("Car Selected", ((Car) event.getObject()).getId());
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+}
